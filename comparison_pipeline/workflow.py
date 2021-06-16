65427eb180fb5faf3435a7a5e56b97da62ee0cd9
Stephen Watts <hello@stephen.ac>
2021-06-17 09:37:48 +1000

    Dynamically build nextflow config file

--- comparison_pipeline/workflow.py
+++ comparison_pipeline/workflow.py
@@ -9,23 +9,42 @@ from . import log
 docker_uri = '843407916570.dkr.ecr.ap-southeast-2.amazonaws.com/comparison_pipeline:0.0.1'
 
 
-def run(inputs_fp, output_dir, resume, docker, executor, bucket):
-    # Set workflow configuration
-    args = list()
-    args.append(f'--inputs_fp {inputs_fp}')
-    args.append(f'--output_dir {output_dir}')
-    if resume:
-        args.append('-resume')
-    if docker:
-        args.append(f'-with-docker {docker_uri}')
+def create_configuration(inputs_fp, output_dir, docker, executor):
+    # Get configuration
+    config_lines = list()
+    config_lines.append('// Inputs and outputs')
+    config_lines.append(f'params.inputs_fp = "{inputs_fp}"')
+    config_lines.append(f'params.output_dir = "{output_dir}"')
+    config_lines.append('')
+    config_lines.append('// Executor')
     if executor == 'aws':
-        args.append('-process.executor awsbatch')
-        args.append('-process.queue nextflow-testing-job-queue')
-        args.append(f'-bucket-dir {bucket}')
+        config_lines.append('process.executor = "awsbatch"')
+        config_lines.append('process.queue = "nextflow-testing-job-queue"')
+        config_lines.append('aws.region = "ap-southeast-2"')
     elif executor == 'local':
-        args.append('-process.executor local')
+        config_lines.append('process.executor = "local"')
     else:
         assert False
+    if docker:
+        config_lines.append('docker.enabled = true')
+        config_lines.append(f'process.container = "{docker_uri}"')
+    # Write to disk
+    output_fp = output_dir / 'nextflow.config'
+    with output_fp.open('w') as fh:
+        print(*config_lines, sep='\n', file=fh)
+    return output_fp
+
+
+def run(inputs_fp, output_dir, resume, docker, executor, bucket):
+    # Set workflow configuration
+    config_fp = create_configuration(inputs_fp, output_dir, docker, executor)
+    # Set command line arguments
+    args = list()
+    args.append(f'-c {config_fp}')
+    if resume:
+        args.append('-resume')
+    if executor == 'aws':
+        args.append(f'-bucket-dir {bucket}')
     args_str = ' '.join(args)
     # Prepare full command
     log.task_msg_title('Executing workflow')
